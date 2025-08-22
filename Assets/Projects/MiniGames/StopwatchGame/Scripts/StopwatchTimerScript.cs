47e4f81d1c0b4f37912a8f896f53a296dc388d6b
kokutoupan <kokutou2004@gmail.com>
2025-08-22 19:02:55 +0900

    タイマーゲームにポイント付与

--- Assets/Projects/MiniGames/StopwatchGame/Scripts/StopwatchTimerScript.cs
+++ Assets/Projects/MiniGames/StopwatchGame/Scripts/StopwatchTimerScript.cs
@@ -14,9 +14,14 @@ namespace Project.Minigame.Stopwatch
 
         public TMP_Text ButtonText;
 
+        // playerDataへの参照
+        [SerializeField] private Projects.Core.PlayerData playerData;
+
         private bool isClick = false;
 
         private bool isbActive = false;
+
+        private double goalTime = 0.0f;
 
         public void OnStart()
         {
@@ -29,7 +34,8 @@ namespace Project.Minigame.Stopwatch
         {
             ButtonText.text = "Start";
 
-            goaltimeText.text = (Random.Range(8, 20) + "秒で止めろ");
+            goalTime = Random.Range(8, 20);
+            goaltimeText.text = (goalTime + "秒で止めろ");
 
         }
 
@@ -52,8 +58,33 @@ namespace Project.Minigame.Stopwatch
 
                 if (Input.GetMouseButtonDown(0))
                 {
+                    // stopしたときの処理
                     timeText.text = countup.ToString("f2");
                     isClick = true;
+
+                    double result = countup - goalTime;
+                    if (result < 1e-6)
+                    {
+                        // 目標時間より早い場合の処理
+                        float tmp = 1.0f + (float)result;
+                        int score = (int)Mathf.Max(0.0f, tmp * 50);
+                        Debug.Log("目標時間より早い: " + score);
+                        playerData.AddPoint(score); // スコア加算
+                    }
+                    else if (result > 1e-6)
+                    {
+                        // 目標時間より遅い場合の処理
+                        float tmp = 1.0f - (float)result;
+                        int score = (int)Mathf.Max(0.0f, tmp * 50);
+                        Debug.Log("目標時間より遅い: " + score);
+                        playerData.AddPoint(score); // スコア加算
+                    }
+                    else
+                    {
+                        // 目標時間に近い場合の処理
+                        Debug.Log("ピッタリ");
+                        playerData.AddPoint(100); // スコア加算
+                    }
                 }
             }
         }
